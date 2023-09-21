ca2724e5edf519ad86becf9257e388b1024ce136
nabilpatel4012 <nabilpatel4012@gmail.com>
2023-09-21 21:24:41 +0530

    no commit message everytime XD

--- main.go
+++ main.go
@@ -6,44 +6,42 @@ import (
 
 func main() {
 
-  var myVar = "Hello"
-  myVar2 := "Hii"
-  name := "Nabil"
-  var num int = 4
-  const myConstant = 99
-  fmt.Println(myVar)
-  fmt.Println(myVar2)
-  fmt.Println(myConstant)
-  fmt.Printf("Hello World!\n")
+	var myVar = "Hello"
+	myVar2 := "Hii"
+	name := "Nabil"
+	var num int = 4
+	const myConstant = 99
+	fmt.Println(myVar)
+	fmt.Println(myVar2)
+	fmt.Println(myConstant)
+	fmt.Printf("Hello World!\n")
 
-  printMe()
-  myFunctionWithInput(myVar)
-  squaredValue := myFunctionWithReturnType(num)
-  fmt.Println(squaredValue)
-  myFunctionWithMultipleReturnType(num,name)
+	printMe()
+	myFunctionWithInput(myVar)
+	squaredValue := myFunctionWithReturnType(num)
+	fmt.Println(squaredValue)
+	myFunctionWithMultipleReturnType(num, name)
 
 }
-
 
 //Functions
 
-//Without return-type and input arguments
+// Without return-type and input arguments
 func printMe() {
-  fmt.Println("I am printed!!!")
+	fmt.Println("I am printed!!!")
 }
 
 func myFunctionWithInput(value string) {
-  fmt.Println(value)
+	fmt.Println(value)
 }
 
 func myFunctionWithReturnType(value int) int {
-  var square = value*value
-  return square
+	var square = value * value
+	return square
 }
 
 func myFunctionWithMultipleReturnType(value int, name string) (int, string) {
-  var square = value*value
-  myMessage := "Hello Nabil"
-  return square,myMessage
+	var square = value * value
+	myMessage := "Hello Nabil"
+	return square, myMessage
 }
-
